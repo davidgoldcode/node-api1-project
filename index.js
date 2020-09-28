88360269d41d020c3daa1691d7cea2a76589c09b
David Gold <davidgold@hey.com>
2020-09-28 16:32:54 -0400

    making progress

--- index.js
+++ index.js
@@ -5,24 +5,49 @@ const server = express();
 
 server.use(express.json());
 
-const users = [
-  { id: shortid.generate(), name: "Jane Doe", bio: "Not Tarzans Wife" },
-];
+const users = [{ id: 1, name: "Jane Doe", bio: "Not Tarzans Wife" }];
+let nextId = 2;
 
 server.get("/api/users", (req, res) => {
   res.status(200).json({ data: users });
 });
+// don't know how to show error
 
 server.post("/api/users", (req, res) => {
   const data = req.body;
-  const { name, bio } = data;
-  if (name !== "" || bio !== "") {
+  const found = Object.keys(data).some((k) => {
+    return data[k] === "";
+  });
+  if (found) {
     res
       .status(400)
       .json({ errorMessage: "Please provide name and bio for the user." });
-  } else {
-    users.push({ id: shortid.generate(), ...data });
+  } else if (found === false) {
+    users.push({ id: nextId++, ...data });
     res.status(201).json({ data, users });
+  } else {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database ",
+    });
+  }
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const changes = req.body;
+  found = users.filter((item) => item.id !== id);
+
+  if (found) {
+    Object.assign(found, changes);
+    res.status(201).json({ data: users });
+  } else if (found === false) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else {
+    res
+      .status(500)
+      .json({ errorMessage: "The user information could not be retrieved." });
   }
 });
 
